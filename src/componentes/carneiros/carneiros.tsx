c137c8b2094809ee6c2acd72ad9b213992b4945b
Victor Laranjeira <victor-laranjeira18@hotmail.com>
2023-08-07 16:44:30 -0300

    Mudança de imagens para carneiros.

--- src/componentes/carneiros/carneiros.tsx
+++ src/componentes/carneiros/carneiros.tsx
@@ -1,33 +1,33 @@
 import React from 'react';
 import styles from  './carneiros.module.css';
-import nature from '../../images/COMPLEXO-NATURE-1500x710.jpg'
-import tropi from '../../images/COMPLEXO-TROPI-1500x710.jpg'
-import exclusive from '../../images/Praia-Exclusive.jpg'
-import cais from '../../images/thumb-cais.jpg'
+import costaAzul from '../../images/DUE_Costa-Azul_Fachada-Studio02-1920x994.jpg'
+import costaDoMar from '../../images/FACHADA-COSTA-DO-MAR-1920x994.jpg'
+
+import dueLogo from '../../images/Due-logo.png'
 
 const Carneiros: React.FC = () => {
 
   const carneirosInfoArray = [
     {
-      image: cais,
+      image: costaAzul,
       resortName: 'VILA COSTA AZUL',
       resortPrice: 'A PARTIR DE R$ 558.500',
       resortSpace: '62,79M² - 175,29M²'
     },
     {
-      image: nature,
+      image: costaDoMar,
       resortName: 'VILA COSTA DO MAR',
       resortPrice: 'A PARTIR DE R$ 556.500',
       resortSpace: '30,27M² - 124,87M²'
     },
     {
-      image: tropi,
+      image: dueLogo,
       resortName: 'NOMAR',
       resortPrice: 'A PARTIR DE R$ 827.400',
       resortSpace: '56,87M² - 90,47M²'
     },
     {
-      image: exclusive,
+      image: dueLogo,
       resortName: 'Lançamento',
       resortPrice: 'FILA DE ESPERA COM CADASTRO',
       resortSpace: '56,87M² - 90,47M²'
